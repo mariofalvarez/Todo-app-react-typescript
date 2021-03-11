ecfa223350acb16f7f61dcc78d24e22cedf9674a
mariofalvarez <mariofalvarezjr@gmail.com>
2021-03-11 12:30:38 -0800

    conflict practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Todo from './components/Todo/Todo';
+
 function App() {
   return (
     <div className="App">
       <p>Diamond Hands!</p>
       <p>Milktea Madnesssss</p>
+      <Todo />
     </div>
   );
 }
---
+++ src/components/Todo/Todo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Todo() {
+    return (
+        <div>
+            <h2>Billy Bobby Molly</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate cupiditate nostrum rerum aperiam cum distinctio libero veniam quaerat accusantium, sunt sit incidunt sed natus placeat doloremque numquam commodi minus. Tenetur.</p>
+        </div>
+    )
+}
+
+export default Todo
